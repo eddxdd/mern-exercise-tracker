3acdf464ce8b6651a5b508fcd9a75b40be56162b
Eduardo Lemos <eddu1930@gmail.com>
2020-03-15 19:45:01 -0400

    Update

--- src/components/homepage.component.js
+++ src/components/homepage.component.js
@@ -6,7 +6,7 @@ export default class Homepage extends Component {
             <div className="container">
                 <h3 className="text-center">Welcome!</h3>
                 <h3 className="text-center">Create an user to log an exercise.</h3>
-                <a href="https://github.com/eddxdd"><i className="fa fa-github text-center" style={{display:"block", fontSize:"50px", color:"#212529", textDecoration:"none"}}></i></a>
+                <a href="https://github.com/eddxdd/mern-exercise-tracker"><i className="fa fa-github text-center" style={{display:"block", fontSize:"50px", color:"#212529", textDecoration:"none"}}></i></a>
                 <p className="text-center">This app was created using the MERN stack. More info on my GitHub.</p>
             </div>
         );
