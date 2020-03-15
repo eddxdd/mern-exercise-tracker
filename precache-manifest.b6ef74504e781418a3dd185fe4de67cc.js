82f905116e25a42c90f96481efd3c6838e6d10bb
Eduardo Lemos <eddu1930@gmail.com>
2020-03-15 19:58:53 -0400

    Updates

---
+++ precache-manifest.b6ef74504e781418a3dd185fe4de67cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1596d052bc533a7b21f0c0ce4b9f8d3",
+    "url": "/mern-exercise-tracker/index.html"
+  },
+  {
+    "revision": "a6e4e6bfe66c1c81b33e",
+    "url": "/mern-exercise-tracker/static/css/2.2be46016.chunk.css"
+  },
+  {
+    "revision": "6a0a08e73d94646d6344",
+    "url": "/mern-exercise-tracker/static/css/main.1dd9ae4f.chunk.css"
+  },
+  {
+    "revision": "a6e4e6bfe66c1c81b33e",
+    "url": "/mern-exercise-tracker/static/js/2.b043e23b.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/mern-exercise-tracker/static/js/2.b043e23b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0a08e73d94646d6344",
+    "url": "/mern-exercise-tracker/static/js/main.425cbe6c.chunk.js"
+  },
+  {
+    "revision": "2a9a0deff62c558bc3f2",
+    "url": "/mern-exercise-tracker/static/js/runtime-main.e109c014.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mern-exercise-tracker/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mern-exercise-tracker/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mern-exercise-tracker/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mern-exercise-tracker/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mern-exercise-tracker/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
